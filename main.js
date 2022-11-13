2e1816a527fd66321dffd444741c005376ec7653
Ryan <asdfasdf>
2022-11-13 22:14:59 +0800

    finished

--- main.js
+++ main.js
@@ -9,62 +9,78 @@ let title = document.createElement("h1");
 title.innerText = "Super Sketchy";
 container.append(title);
 
+let enterNumberHeader = document.createElement("h2");
+enterNumberHeader.innerText = "Choose a number between 2 and 100";
+enterNumberHeader.style.fontSize = "16px";
+container.append(enterNumberHeader);
+
 let inputBox = document.createElement("input");
 inputBox.setAttribute("type", "text");
 inputBox.className = "inputBox";
-inputBox.value = "20";
-//inputBox.onchange = createSizeOfBoard(this.value);
-inputBox.placeholder = "Enter a # 2-100";
+inputBox.id = "inputBox";
+inputBox.value = "";
 inputBox.style.width = "100px";
+inputBox.style.maxWidth = "100px";
 inputBox.style.height = "25px";
 container.append(inputBox);
 
 let chooseSizeBtn = document.createElement("button");
-chooseSizeBtn.className = "button";
+chooseSizeBtn.className = "chooseSizeBtn";
+chooseSizeBtn.id = "chooseSizeBtn";
 chooseSizeBtn.innerText = "Choose Size";
 container.append(chooseSizeBtn);
 
 let resetBtn = document.createElement("button");
-resetBtn.className = "button";
+resetBtn.className = "resetBtn";
 resetBtn.innerText = "Reset";
 container.append(resetBtn);
 
 document.querySelectorAll("button").forEach((button) => {
   button.style.width = "100px";
+  button.style.maxWidth = "100px";
   button.style.height = "30px";
 });
+
+// @ts-ignore
 
 function createBoard(size) {
   let board = document.createElement("div");
   board.className = "board";
+  board.id = "board";
   board.style.height = "500px";
   board.style.width = "500px";
   board.style.borderStyle = "solid";
-  board.style.borderWidth = "4px";
+  board.style.borderWidth = "6px";
   board.style.borderColor = "black";
   board.style.display = "grid";
   board.style.gridTemplateColumns = `repeat(${size},1fr)`;
   board.style.gridTemplateRows = `repeat(${size},1fr)`;
-
   container.append(board);
-  let amount = size * size;
-  for (let i = 0; i < amount; i++) {
-    let box = document.createElement("div");
-    box.addEventListener("mouseover", function () {
-      this.style.backgroundColor = "black";
-    });
-    box.style.backgroundColor = "blue";
 
-    board.append(box);
-  }
-}
-createBoard(35);
-
-function createSizeOfBoard(input) {
-  if (input >= 2 && input <= 100) {
-    createBoard(input);
+  if (size < 2 || size > 99) {
+    console.log("wrong number");
   } else {
-    console.log("error");
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+      let box = document.createElement("div");
+      box.className = "boxes";
+      box.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "black";
+      });
+      box.style.backgroundColor = "white";
+      board.append(box);
+    }
   }
 }
-createSizeOfBoard(input);
+function clearBoard() {
+  document.getElementById("board")?.remove();
+}
+
+chooseSizeBtn.addEventListener("click", () => {
+  const btn = document.getElementById("inputBox");
+  const size = btn.value;
+  clearBoard();
+  createBoard(parseInt(size));
+});
+
+resetBtn.addEventListener("click", clearBoard);
